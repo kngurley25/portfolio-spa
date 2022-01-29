e106badc5fc57b9449134dc54d84d73dc21eefad
kngurley <kngurley25@gmail.com>
2022-01-29 12:25:57 -0600

    add initial styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>Kristin Gaito</h1>
+                <ul>
+                    <h3>About Me</h3>
+                    <h3>Portfolio</h3>
+                    <h3>Contact</h3>
+                    <h3>Resume</h3>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
