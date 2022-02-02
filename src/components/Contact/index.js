98ca12b9bb3f534914eb31a2e5cb499623d35d5c
kngurley <kngurley25@gmail.com>
2022-02-02 17:02:12 -0600

    add use state to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+        alert('Form submitted. Thank you!');
+        // to complete with back-end development
+    }
+
     return (
         <section>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label htmlFor="name">Name</Form.Label>
-                    <Form.Control type="text" name="name" placeholder="Enter name"></Form.Control>
+                    <Form.Control type="text" name="name" defaultValue={name} onChange={handleChange} placeholder="Enter name"></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email"></Form.Control>
+                    <Form.Control type="email" name="email" defaultValue={email} onChange={handleChange} placeholder="Enter email"></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label htmlFor="message">Message</Form.Label>
-                    <Form.Control as="textarea" rows={5} type="text" name="message" placeholder="Enter message"></Form.Control>
+                    <Form.Control as="textarea" rows={5} type="text" name="message" defaultValue={message} onChange={handleChange} placeholder="Enter message"></Form.Control>
                 </Form.Group>
                 <button type="submit">Submit</button>
             </Form>
