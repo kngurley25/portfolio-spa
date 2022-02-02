0f81ec03cfb0a2e431da7cd50984b8620733c7d6
kngurley <kngurley25@gmail.com>
2022-02-02 16:41:41 -0600

    misc formatting updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,8 @@ function About() {
                         I have an engineering background and am excited to jump into the world of programming! 
                     </p>
                     <p>
-                        I graduated from Kansas State University with a degree in architectural engineering and am a licensed professional electrical engineer. 
+                        I graduated from Kansas State University with a degree in architectural engineering. 
+                        For over six years, I have worked in the engineering and construction field and have obtained my professional engineering license as an electrical engineer. 
                         I'm planning to transition from creating engineering construction documents to developing web applications with the training I've received from KU Coding Bootcamp. 
                         KU Coding Bootcamp has provided me exposure to front-end and back-end web development tools and concepts. 
                         I'm looking forward to building upon on this knowledge as I continue with a career in web development.
