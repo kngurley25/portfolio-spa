adf03339f95d37a6b822134c36c43e9255253b8a
kngurley <kngurley25@gmail.com>
2022-02-02 17:37:28 -0600

    add contact form input validation and error message

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Input valid email address.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`A ${e.target.name} is required for contact form.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
-        alert('Form submitted. Thank you!');
+        if (!errorMessage) {
+
+            alert('Form submitted. Thank you!');
+        }
         // to complete with back-end development
     }
 
@@ -22,16 +44,21 @@ function ContactForm() {
             <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label htmlFor="name">Name</Form.Label>
-                    <Form.Control type="text" name="name" defaultValue={name} onChange={handleChange} placeholder="Enter name"></Form.Control>
+                    <Form.Control type="text" name="name" defaultValue={name} onBlur={handleChange} placeholder="Enter name"></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control type="email" name="email" defaultValue={email} onChange={handleChange} placeholder="Enter email"></Form.Control>
+                    <Form.Control type="email" name="email" defaultValue={email} onBlur={handleChange} placeholder="Enter email"></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label htmlFor="message">Message</Form.Label>
-                    <Form.Control as="textarea" rows={5} type="text" name="message" defaultValue={message} onChange={handleChange} placeholder="Enter message"></Form.Control>
+                    <Form.Control as="textarea" rows={5} type="text" name="message" defaultValue={message} onBlur={handleChange} placeholder="Enter message"></Form.Control>
                 </Form.Group>
+                {errorMessage && (
+                    <div>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </Form>
         </section>
