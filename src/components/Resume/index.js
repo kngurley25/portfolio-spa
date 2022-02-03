9dbb02ffefc2689a6cce5b2a27d4c8e7aea988d0
kngurley <kngurley25@gmail.com>
2022-02-02 19:56:30 -0600

    add resume section style

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 const resume = require('../../assets/pdf/resume.pdf');
 
 function Resume() {
+
+    const [languages] = useState([
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ]);
+    const [applications] = useState([
+        'Node.js',
+        'Express.js',
+        'MySql with Sequelize',
+        'MongoDB with Mongoose',
+        'React'
+    ])
     return (
         <section className="resume-section">
             <div>
-                <a href={resume} download="KristinGaito_Resume">Download</a>
+                <a href={resume} download="KristinGaito_Resume">Download PDF</a>
             </div>
-            <div>
-                <h5>Technical Skills</h5>
-                <ul>
-                    <li>sample</li>
-                    <li>sample</li>
-                </ul>
+            <div className="skill-list">
+                <h4>Technical Skills</h4>
+                <div>
+                    <h5>Languages</h5>
+                    {languages.map((skill) => {
+                        return <li className="skill">{skill}</li>
+                    })}
+                </div>
+                <div>
+                    <h5>Applications</h5>
+                    {applications.map((skill) => {
+                        return <li className="skill">{skill}</li>
+                    })}
+                </div>
             </div>
         </section>
     );
