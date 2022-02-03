18ddd29b900f9e20cec2ad76788b8321f3d8d6f9
kngurley <kngurley25@gmail.com>
2022-02-02 21:27:18 -0600

    update with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Project from "./components/Project";
