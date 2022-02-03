c228e035f94a90e7dffbcdef19ca592cec2e19a0
kngurley <kngurley25@gmail.com>
2022-02-02 20:13:55 -0600

    add icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+import { GoMarkGithub } from 'react-icons/go';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { BsStackOverflow } from 'react-icons/bs';
+
 function Footer() {
     return (
         <footer>
             <div>
-                <a href="https://github.com/kngurley25" target="_blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/kristin-gaito-gurley-pe-69041569/" target="_blank">LinkedIn</a>
+                <a href="https://github.com/kngurley25" target="_blank"><GoMarkGithub></GoMarkGithub></a>
+                <a href="https://www.linkedin.com/in/kristin-gaito-gurley-pe-69041569/" target="_blank"><AiFillLinkedin></AiFillLinkedin></a>
+                <a href="https://stackoverflow.com/users/16674518/kristin-gaito" target="_blank"><BsStackOverflow></BsStackOverflow></a>
             </div>
         </footer>
     );
